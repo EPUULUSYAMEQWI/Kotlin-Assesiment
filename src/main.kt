aab0e4e1ad8c918194b8580ae00823d60c2785f9
EPUULUSYAMEQWI <epuulucyamekwi@gmail.com>
2022-03-10 00:53:26 -0500

    first commit

---
+++ src/main.kt
@@ -0,0 +1,25 @@
+fun main() {
+    println(y)
+var x=parameter(name:"lucy",age:"21")
+    println(x)
+    println("length")
+    count()
+
+
+
+}
+
+fun name( "CodeHive":String){
+    var y = ("name[5],.toString [6], name[7] ,name[8]")
+    return y
+}
+fun parameter=(name:String,age:Int):String{
+    var x = ("Hi my name is $name and I am "$age years old")
+    return(x)
+}
+
+fun name(name:String):Int{
+    var z=("kenya")
+    return(name.count())
+
+}
